352dfb5eba6bce2bfdf3f938633402352068b717
Flavia Dessoldi <flavia.dessoldi@gmail.com>
2019-07-08 18:22:01 -0300

    alteracoes no fetch de login

--- frontend/cadastrar.js
+++ frontend/cadastrar.js
@@ -1,11 +1,13 @@
     //POST ADICIONAR PET
     const containerCadastrar = document.querySelector('#cadastroPet')
+
+    const usuarioId = document.querySelector('#data-id')
     
     const botao = document.querySelector('#botaoPet')
     
     botao.addEventListener("click", (criarPet) => {
       criarPet.preventDefault()
-    
+      
       const nome = document.querySelector("#nomePet").value
       const especie = document.querySelector("#especiePet").value
       const raca = document.querySelector("#racaPet").value
@@ -38,30 +40,30 @@
             const textoSucesso = document.querySelector('h4')
             textoSucesso.innerHTML = ('O Pet foi cadastro foi cadastrado com sucesso!')
     
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card horizontal')
-            card.innerHTML = `
-          <div class="card-image">
+      //       const card = document.createElement('div')
+      //       card.setAttribute('class', 'card horizontal')
+      //       card.innerHTML = `
+      //     <div class="card-image">
            
-          </div>
-          <div class="card-stacked">
-            <div class="card-content">
-              <p>Nome: ${pet.nome} 
-              Especie: ${pet.espécie},
-        Gênero: ${pet.genero},
-        Porte:${pet.porte}
-        Cor do pêlo: ${pet.pelo},
-        Outras Caracteristicas: ${pet.outrasCaracteristicas},
-        Visto por último na data: ${pet.data},
-        Visto por último no local:${pet.local}
-      </p>
-            </div>`
+      //     </div>
+      //     <div class="card-stacked">
+      //       <div class="card-content">
+      //         <p>Nome: ${pet.nome} 
+      //         Especie: ${pet.espécie},
+      //   Gênero: ${pet.genero},
+      //   Porte:${pet.porte}
+      //   Cor do pêlo: ${pet.pelo},
+      //   Outras Caracteristicas: ${pet.outrasCaracteristicas},
+      //   Visto por último na data: ${pet.data},
+      //   Visto por último no local:${pet.local}
+      // </p>
+      //       </div>`
     
-            containerCadastrar.appendChild('card')
+      //       containerCadastrar.appendChild('card')
+          })
+          .catch((erro) => {
+            console.log(erro)
           })
         })
-        .catch((erro) => {
-          console.log(erro)
-        })
-    
+          
      
