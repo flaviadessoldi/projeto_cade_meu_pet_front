01ae0cd0dead8ee8fed03ff78dd73d7db32d48c5
Flavia Dessoldi <flavia.dessoldi@gmail.com>
2019-07-11 19:53:33 -0300

    configuracao do card do pet

--- frontend/buscar.js
+++ frontend/buscar.js
@@ -14,9 +14,9 @@ botaoBuscar.addEventListener("click", (buscarPet) => {
   const outrasCaracteristicasBuscar = document.querySelector("#caracteristicasPetBuscar").value.toLowerCase()
   const dataBuscar = document.querySelector("#calendario").value.toLowerCase()
   const localBuscar = document.querySelector("#localPet2").value.toLowerCase()
-  const petBuscar = {
-    nomeBuscar, especieBuscar, racaBuscar, generoBuscar, porteBuscar, corBuscar, outrasCaracteristicasBuscar, dataBuscar, localBuscar, porteBuscar
-  }
+  // const petBuscar = {
+  //   nomeBuscar, especieBuscar, racaBuscar, generoBuscar, porteBuscar, corBuscar, outrasCaracteristicasBuscar, dataBuscar, localBuscar, porteBuscar
+  // }
 
 
   fetch(`http://localhost:3000/busca/pets/?especie=${especieBuscar}&raca=${racaBuscar}&genero=${generoBuscar}&${porteBuscar}&cor=${corBuscar}&data=${dataBuscar}&local=${localBuscar}`)
@@ -26,59 +26,119 @@ botaoBuscar.addEventListener("click", (buscarPet) => {
     })
     .then((usuarios) => {
       console.log(usuarios)
-           
-      if (petBuscar) {
-        return usuarios
-      }
 
-      usuarios.forEach(pet =>{
+      usuarios.forEach(user => {
 
-      const cardBody = document.querySelector('#buscarPet')
+        console.log(user.nome)
+        console.log(user.email)
+        console.log(user.telefone)
 
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card horizontal')
+          user.pets.forEach(pet => {
+                                     
+          const cardBody = document.querySelector('#buscarPet')
+
+          const card = document.createElement('div')
+          card.setAttribute('class', 'card horizontal')
+
+          const cardImage = document.createElement('div')
+          cardImage.setAttribute('class', 'card-image')
+
+          const img = document.createElement('img')
+          img.setAttribute('src', pet.foto)
+          
+
+          const cardStacked = document.createElement('div')
+          cardStacked.setAttribute('class', 'card-stacked')
+
+          const cardContent = document.createElement('div')
+          cardContent.setAttribute('class', 'card-content')
+
+          const titulo = document.createElement('h6')
+          titulo.innerHTML = 'Dados do Pet'
+
+          const p1 = document.createElement('p')
+          p1.innerHTML =`Nome: ${pet.nome}`
+          
+          const p2 = document.createElement('p')
+          p2.innerHTML =`Espécie: ${pet.especie}`
+          
+          const p3 = document.createElement('p')
+          p3.innerHTML =`Gênero: ${pet.genero}`
+
+          const p4 = document.createElement('p')
+          p4.innerHTML =`Cor do pêlo: ${pet.cor}`
+
+          const p5 = document.createElement('p')
+          p5.innerHTML =`Outras Características: ${pet.outrasCaracteristicas}`
+
+          const p7 = document.createElement('p')
+          p7.innerHTML =`Visto por último na data: ${pet.data}`
+
+          const p8 = document.createElement('p')
+          p8.innerHTML =`Visto por último no local: ${pet.local}`
+          
+        const tituloUser = document.createElement('h6')
+        tituloUser.innerHTML = 'Quem encontrou esse Pet:'
         
-        const cardImage = document.createElement('div')
-        card.setAttribute('class', 'card-image')
-
-        const img = document.createElement('img')
-        card.setAttribute('src', usuarios.pets.foto)
-
-        const cardStacked = document.createElement('div')
-        cardStacked.setAttribute('class', 'card-stacked')
-
-        const cardContent = document.createElement('div')
-        card.setAttribute('class', 'card-content')
-
-        const p = document.createElement('p')
-        p.innerHTML=`Nome: ${pet.nome}, 
-        Especie: ${pet.especie},
-  Gênero: ${pet.genero},
-  Porte:${usuarios.pets.porte}
-  Cor do pêlo: ${usuarios.pets.pelo},
-  Outras Caracteristicas: ${usuarios.pets.outrasCaracteristicas},
-  Visto por último na data: ${usuarios.pets.data},
-  Visto por último no local:${usuarios.pets.local},
-  Dados de quem cadasrou o Pet:
-  Nome:${usuarios.nome},
-  Email:${usuarios.email},
-  Telefone ${usuarios.telefone}`
+        const p1User = document.createElement('p')
+        p1User.innerHTML =`Nome: ${user.nome}` 
+        
+        const p2User = document.createElement('p')
+        p2User.innerHTML =`Email: ${user.email}`
+        
+        const p3User = document.createElement('p')
+        p3User.innerHTML =`Telefone: ${user.telefone}`
         
 
-        cardBody.appendChild(card)
-        card.appendChild(cardImage)
-        cardImage.appendChild(img)
-        cardStacked.appendChild(cardContent)
-        cardContent.appendChild(p)
+          
+          // Visto por último na data: ${pet.data},
+          // Visto por último no local:${pet.local}
+          // Quem encontrou esse Pet: ${user.nome}, 
+          // Email: ${user.email},
+          // Telefone: ${user.telefone}`
+
+
+          cardBody.appendChild(card)
+          card.appendChild(cardImage)
+          cardImage.appendChild(img)
+          card.appendChild(cardStacked)
+          cardStacked.appendChild(cardContent)
+          cardContent.appendChild(titulo)
+          cardContent.appendChild(p1)
+          cardContent.appendChild(p2)
+          cardContent.appendChild(p3)
+          cardContent.appendChild(p4)
+          cardContent.appendChild(p5)
+          cardContent.appendChild(p7)
+          cardContent.appendChild(p8)
+         cardContent.appendChild(tituloUser)
+        cardContent.appendChild(p1User)
+        cardContent.appendChild(p2User)
+        cardContent.appendChild(p3User)
+
+        })
+
 
       })
     })
- })
+})
   .catch((erro) => {
     console.log(erro)
   })
 
-  
+
+
+
+
+
+// Dados de quem cadasrou o Pet:
+// Nome:${usuarios.nome},
+// Email:${usuarios.email},
+// Telefone ${usuarios.telefone}`
+
+
+
+
 
 
 
